9992cc3960de52b8dccbedbf9214c25c2ca57706
ismailDüzel <iduzel49@gmail.com>
2020-12-25 10:32:27 +0100

    dalClass and dalClass2 added

---
+++ src/com/duzel/dalClass.java
@@ -0,0 +1,8 @@
+package com.duzel;
+
+public class dalClass {
+    public static void main(String[] args) {
+        System.out.println("Dal1 Class is added");
+    }
+
+}
---
+++ src/com/duzel/dalClass2.java
@@ -0,0 +1,7 @@
+package com.duzel;
+
+public class dalClass2 {
+    public static void main(String[] args) {
+        System.out.println("Dal 2 class eklendi");
+    }
+}
